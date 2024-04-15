588544d049a1cee8710f60d6bbce5ecf947a8c75
aashishrl <iashish9800@gmail.com>
2024-04-15 14:59:36 +0545

    newFeat: Language translatio to english added

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer');
+const translate = require('translate-google');
 
-async function run (){
-    const browser = await puppeteer.launch()
-    const page = await browser.newPage()
-    await page.goto('https://www.weeronline.nl/')
-
-    await browser.close()
+async function translateText(text) {
+    try {
+        const translatedText = await translate(text, { to: 'en' });
+        return translatedText;
+    } catch (error) {
+        console.error('Error translating content:', error);
+        return text; // Return original text in case of error
+    }
 }
 
-run()
+async function scrapeAndTranslate() {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('https://www.weeronline.nl/');
+
+    // Scrape text from the webpage
+    const scrapedTitle = await page.evaluate(() => {
+        return document.querySelector('.fc-dialog-headline').innerText;
+    });
+    console.log(scrapedTitle);
+    // Translate the scraped text
+    const translatedText = await translateText(scrapedTitle);
+    console.log('Translated text:', translatedText);
+
+    await browser.close();
+}
+
+scrapeAndTranslate();
